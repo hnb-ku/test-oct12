be644f956b929f246618a99d61cf4ad5408d7d4e
Joe <xyz.hunabku@gmail.com>
2021-10-12 03:44:18 +0800

    Add files via upload

---
+++ javascripts/discourse/api-initializers/discomarks.js
@@ -0,0 +1,60 @@
+import { apiInitializer } from "discourse/lib/api";
+import loadScript from "discourse/lib/load-script";
+
+export default apiInitializer("0.8", (api) => {
+  const currentUser = api.getCurrentUser();
+  if (!currentUser) return;
+
+  const watermarkOpts = {
+    text: "zpfei.ink",
+  };
+
+  const canvas = document.createElement("canvas");
+  const img = document.createElement("img");
+
+  img.src = settings.default_site_watermark;
+
+  console.log(img);
+  const ctx = canvas.getContext("2d");
+
+  api.addComposerUploadProcessor(
+    { action: "automaticWatermarks" },
+    {
+      automaticWatermarks: (data, options) => {
+        let p = new Promise((resolve, reject) => {
+          const file = data.files[data.index];
+
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            img.src = reader.result;
+            img.onload = () => {
+              const fontSize = img.width / 20;
+              canvas.width = img.width;
+              canvas.height = img.height;
+
+              ctx.drawImage(img, 0, 0);
+              ctx.font = `${fontSize}px Arial`;
+              ctx.fillStyle = "red";
+              ctx.fillText(
+                watermarkOpts.text,
+                canvas.width * 0.015,
+                canvas.height - canvas.height * 0.02
+              );
+              canvas.toBlob((canvasBlob) => {
+                const watermarked = new File([canvasBlob], file.name, {
+                  type: file.type,
+                });
+
+                data.files[data.index] = watermarked;
+                resolve(data);
+              });
+            };
+          };
+        });
+
+        return p;
+      },
+    }
+  );
+});
