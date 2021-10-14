20ea886dbc49810dfce889fd06d3f6c7e102a3cf
Joe <xyz.hunabku@gmail.com>
2021-10-14 10:40:00 +0800

    Update discomarks.js

--- javascripts/discourse/api-initializers/discomarks.js
+++ javascripts/discourse/api-initializers/discomarks.js
@@ -4,78 +4,38 @@ export default apiInitializer("0.8", (api) => {
   const currentUser = api.getCurrentUser();
   if (!currentUser) return;
 
-  const watermarkOpts = {
-    text: "zpfei.ink",
-  };
-
-  const positionLeft = 5;
-  const positionTop = 5;
-
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  const img = document.createElement("img");
-
-  const downloadedImg = new Image();
-  downloadedImg.crossOrigin = "Anonymous";
-  downloadedImg.src = settings.default_site_watermark;
-
   api.addComposerUploadProcessor(
     { action: "automaticWatermarks" },
     {
       automaticWatermarks: (data, options) => {
-        const t0 = performance.now();
-
         let p = new Promise((resolve, reject) => {
           const file = data.files[data.index];
-
+       
           const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => {
-            img.src = reader.result;
-            img.onload = () => {
-              const imageWidth = img.width;
-              const imageHeight = img.height;
 
-              const fontSize = imageWidth / 20;
+          reader.onload = function (e) {
+            var exifObj = piexif.load(e.target.result);
+            var exifStr = piexif.dump(exifObj);
+            var inserted = piexif.insert(exifStr, e.target.result);
 
-              const watermarkAspectRatio =
-                downloadedImg.width / downloadedImg.height;
-              const watermarkWidth = img.width / 3;
-              const watermarkHeight = watermarkWidth / watermarkAspectRatio;
-
-              canvas.width = imageWidth;
-              canvas.height = imageHeight;
-
-              ctx.drawImage(img, 0, 0);
-              ctx.font = `${fontSize}px Arial`;
-              ctx.fillStyle = "red";
-              ctx.drawImage(
-                downloadedImg,
-                positionLeft,
-                positionTop,
-                watermarkWidth,
-                watermarkHeight
-              );
-
-              ctx.fillText(
-                watermarkOpts.text,
-                canvas.width * 0.015,
-                canvas.height - canvas.height * 0.02
-              );
-              canvas.toBlob((canvasBlob) => {
-                const watermarked = new File([canvasBlob], file.name, {
+            fetch(inserted)
+              .then((res) => res.blob())
+              .then((blob) => {
+                const newfile = new File([blob], file.name, {
                   type: file.type,
                 });
 
-                data.files[data.index] = watermarked;
-                const t1 = performance.now();
-                console.log(
-                  `Call to doSomething took ${t1 - t0} milliseconds.`
-                );
+                console.log(file);
+                console.log(newfile);
+
+                data.files[data.index] = newfile;
+
                 resolve(data);
               });
-            };
           };
+          reader.readAsDataURL(file);
+
+        
         });
 
         return p;
@@ -83,3 +43,5 @@ export default apiInitializer("0.8", (api) => {
     }
   );
 });
+
+
